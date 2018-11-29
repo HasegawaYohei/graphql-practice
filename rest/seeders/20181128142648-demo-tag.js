0ac9753277a2d64e288cac2cc7e0a64a2005a483
Hasegawa Yohei <yoheybrains@gmail.com>
2018-11-29 23:35:27 +0900

    modified: add data to seeder

--- rest/seeders/20181128142648-demo-tag.js
+++ rest/seeders/20181128142648-demo-tag.js
@@ -3,7 +3,84 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('tags', [{
-      name: 'javascript',
+      name: 'C',
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+    },{
+      name: 'C++',
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+    },{
+      name: 'C#',
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+    },{
+      name: 'Objective-C',
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+    },{
+      name: 'Swift',
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+    },{
+      name: 'Java',
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+    },{
+      name: 'Scala',
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+    },{
+      name: 'Kotlin',
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+    },{
+      name: 'Perl',
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+    },{
+      name: 'Python',
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+    },{
+      name: 'PHP',
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+    },{
+      name: 'Ruby',
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+    },{
+      name: 'JavaScript',
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+    },{
+      name: 'TypeScript',
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+    },{
+      name: 'Elm',
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+    },{
+      name: 'Haskell',
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+    },{
+      name: 'Ocaml',
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+    },{
+      name: 'Lisp',
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+    },{
+      name: 'Elixir',
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00',
+    },{
+      name: 'プログラミング',
       created_at: '2018-12-01 00:00:00',
       updated_at: '2018-12-01 00:00:00',
     }], {});
--- rest/seeders/20181128143703-demo-articletags.js
+++ rest/seeders/20181128143703-demo-articletags.js
@@ -2,31 +2,200 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
     return queryInterface.bulkInsert('articletags', [{
       article_id: 1,
       tag_id: 1,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 1,
+      tag_id: 20,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 2,
+      tag_id: 2,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 2,
+      tag_id: 20,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 3,
+      tag_id: 3,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 3,
+      tag_id: 20,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 4,
+      tag_id: 4,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 4,
+      tag_id: 20,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 5,
+      tag_id: 5,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 5,
+      tag_id: 20,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 6,
+      tag_id: 6,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 6,
+      tag_id: 20,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 7,
+      tag_id: 7,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 7,
+      tag_id: 20,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 8,
+      tag_id: 8,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 8,
+      tag_id: 20,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 9,
+      tag_id: 9,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 9,
+      tag_id: 20,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 10,
+      tag_id: 10,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 10,
+      tag_id: 20,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 11,
+      tag_id: 11,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 11,
+      tag_id: 20,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 12,
+      tag_id: 12,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 12,
+      tag_id: 20,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 13,
+      tag_id: 13,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 13,
+      tag_id: 20,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 14,
+      tag_id: 14,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 14,
+      tag_id: 20,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 15,
+      tag_id: 15,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 15,
+      tag_id: 20,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 16,
+      tag_id: 16,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 16,
+      tag_id: 20,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 17,
+      tag_id: 17,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 17,
+      tag_id: 20,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 18,
+      tag_id: 18,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 18,
+      tag_id: 20,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 19,
+      tag_id: 19,
+      created_at: '2018-12-01 00:00:00',
+      updated_at: '2018-12-01 00:00:00'
+    },{
+      article_id: 19,
+      tag_id: 20,
       created_at: '2018-12-01 00:00:00',
       updated_at: '2018-12-01 00:00:00'
     }], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
+    return queryInterface.bulkDelete('articletags', null, {});
   }
 };
