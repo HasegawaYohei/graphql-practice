2bc48edc86c9a87c666e9c7a881bc8f3f1028eac
Hasegawa Yohei <yoheybrains@gmail.com>
2018-11-29 23:35:04 +0900

    fixed: routes.js

--- rest/routes/api.js
+++ rest/routes/api.js
@@ -7,33 +7,37 @@ const wrapper = fn => (req, res, next) => fn (req, res).catch(next);
 
 router.get('/articles', wrapper(async (req, res) => {
   const articles = await models.article.findAll();
-  const response = articles.map(article => ( { uri: `http://localhost:${config.port}/api/article/${article.id}`} ));
+  const response = articles.map(article => ( { uri: `/api/article/${article.id}`} ));
   res.status(200).json(response);
 }));
 
 router.get('/article/:id', wrapper(async (req, res) => {
+  const article = (await models.article.findOne({
+    where: {
+      id: req.params.id
+    }
+  })).toJSON();
+  const response = {
+    ...article,
+    tags: `/api/article/${article.id}/tags`
+  };
+  res.status(200).json(response);
+}));
+
+router.get('/article/:id/tags', wrapper(async (req, res) => {
   const article = await models.article.findOne({
     where: {
       id: req.params.id
     }
   });
-  const response = article.toJSON();
-  res.status(200).json(response);
-}));
-
-router.get('/article/:id/tags', wrapper(async (req, res) => {
-  const articles = await models.article.findOne({
-    where: {
-      id: req.params.id
-    }
-  });
-  const tags = await articles.getTags();
+  const tags = await article.getTags();
+  const response = tags.map(tag => ({ uri: `/api/tag/${tag.id}` }));
   res.status(200).json(tags);
 }));
 
 router.get('/tags', wrapper(async (req, res) => {
   const tags = await models.tag.findAll();
-  const response = tags.map(tag => ( { uri: `http://localhost:${config.port}/api/tag/${tag.id}`} ));
+  const response = tags.map(tag => ( { uri: `/api/tag/${tag.id}`} ));
   res.status(200).json(response);
 }));
 
